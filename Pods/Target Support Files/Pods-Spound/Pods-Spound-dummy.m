02e474a8d467f9f99f371806232a09a2a83be26d
barra14 <andresbarrios@Andress-MacBook-Pro.local>
2017-05-07 14:55:22 -0400

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-Spound/Pods-Spound-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spound : NSObject
+@end
+@implementation PodsDummy_Pods_Spound
+@end
---
+++ Spound/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spound
+//
+//  Created by Andres Barrios on 5/7/17.
+//  Copyright © 2017 Andres Barrios. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController { // Sign in ViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
