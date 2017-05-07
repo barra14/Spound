4affb3158b72d4ef13a8d42a67899ffea2ab197f
barra14 <andresbarrios@Andress-MacBook-Pro.local>
2017-05-07 17:48:56 -0400

    Sign in ui custom classes complete

---
+++ Spound/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Spound
+//
+//  Created by Andres Barrios on 5/7/17.
+//  Copyright © 2017 Andres Barrios. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
---
+++ Spound/login_textfield.swift
@@ -0,0 +1,21 @@
+//
+//  login_textfield.swift
+//  Spound
+//
+//  Created by Andres Barrios on 5/7/17.
+//  Copyright © 2017 Andres Barrios. All rights reserved.
+//
+
+import UIKit
+
+class login_textfield: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
+        //layer.borderWidth = 1.0
+        
+    }
+
+}
